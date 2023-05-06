22cd37e6dbbe6681f4b7443bb0ad75e9b7910ddf
sushanta902 <hacktech902@gmail.com>
2023-05-06 08:54:57 +0545

    first

---
+++ lovelycommerse/src/App.jsx
@@ -0,0 +1,13 @@
+import Landing from './components/Landing'
+import Container from './components/Container'
+const App=()=> {
+
+  return (
+    <>
+     <Landing/>
+     <Container/>
+    </>
+  )
+}
+
+export default App
---
+++ lovelycommerse/src/components/Navbody.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbody() {
+  return (
+    <>
+    <div className="navbody">
+        <div className="landingText">
+            <div className="heading">Click Add Shop</div> 
+            <div className="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit repellendus magni ab eligendi consectetur. Accusamus mollitia exercitationem maxime? Ratione adipisci velit porro tempora aliquid odio molestias consectetur eum inventore impedit. lor</div> 
+            <div>
+                <button>Explore</button>
+                <button disabled="disabled"><a target='_blank' href='https://fb.com/sushanta.neupane.5'>About</a> </button>
+
+            </div>
+        </div>
+        <div className="landingImg"></div>
+    </div>
+    </>
+  )
+}
